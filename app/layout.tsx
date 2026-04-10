9c2c2657f1df57a6a04749f349fa9e5fe7ac049e
ricirahman1 <rici7rahman@gmail.com>
2026-04-10 20:44:59 +0700

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,34 @@
 import "./globals.css";
-import { Inter, Allura } from "next/font/google";
-  
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
 const bodyFont = Inter({
   subsets: ["latin"],
   weight: ["400", "500", "600"],
 });
 
+/* ✅ TAMBAHKAN INI */
+export const metadata = {
+  title: "Undangan Pernikahan Rici & Ines",
+  description: "Kami mengundang Anda ke hari bahagia kami - 31 Mei 2026",
+  openGraph: {
+    title: "Undangan Pernikahan Rici & Ines",
+    description: "31 Mei 2026 | Rici & Ines Wedding",
+    url: "https://rici-ines.vercel.app/", // ganti domain kamu
+    siteName: "Wedding Invitation",
+    images: [
+      {
+        url: "https://rici-ines.vercel.app/bg-cvrr.jpeg", // WAJIB FULL URL
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: "id_ID",
+    type: "website",
+  },
+};
+
+/* ================= LAYOUT ================= */
 export default function RootLayout({
   children,
 }: {
